9bc29ec340cfc6c0e07304139d1b52d10827a86e
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2019-01-13 11:02:44 -0500

    setup basic express app, connected to db, rendering Hello World

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+// const bodyParser = require('body-parser')
+// const methodOverride = require('method-override');
+// const logger = require('morgan');
+
+// Database setup
+mongoose.Promise = global.Promise;
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true }
+);
+
+const connection = mongoose.connection;
+
+connection.on('open', () => {
+    console.log('Mongoose Connected Successfully');
+});
+
+connection.on('error', error => {
+    console.log(error);
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log('App is up and running on port ' + PORT);
+});
